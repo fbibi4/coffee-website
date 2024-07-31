57efd788b35a5ef50bdf3a8d6a1d2352705d3fe9
fbibi4 <127233513+fbibi4@users.noreply.github.com>
2024-07-31 20:01:22 +0100

    removed some errors

--- coffee-website/src/App.js
+++ coffee-website/src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./components/pages/home";
+import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Repair from "./components/pages/Repair";
 import Service from "./components/pages/Service";
 import RefurbishedMachines from "./components/pages/RefurbishedMachines";
 import Parts from "./components/pages/Parts";
 import Contact from "./components/pages/Contact";
-import Cancel from "./components/pages/Cancel";
-import Success from "./components/pages/Success";
 import Store from "./components/pages/Store";
 
 function App() {
@@ -23,9 +21,7 @@ function App() {
         <Route path="/shop/refurbished" element={<RefurbishedMachines />} />;
         <Route path="/shop/parts" element={<Parts />} />;
         <Route path="/contact" element={<Contact />} />;
-        <Route path="/Store" element={<Store />} />;
-        <Route path="/Success" element={<Success />} />;
-        <Route path="/Cancel" element={<Cancel />} />;
+        <Route path="/store" element={<Store />} />;
       </Routes>
     </Router>
   );
